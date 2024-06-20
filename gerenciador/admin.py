40dbaf70e5a9c932680d17686a99a4f9d8f4a886
Aluno da Infinity School <aluno@3D.local>
2024-06-20 15:24:05 -0600

    version 1.0.6

--- gerenciador/admin.py
+++ gerenciador/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Cursos,Alunos
 # Register your models here.
+
+class AdminCursos(admin.ModelAdmin):
+    ...
+
+@admin.register(Alunos)
+class AdminAlunos(admin.ModelAdmin):
+    ...
+
+admin.site.register(Cursos,AdminCursos)
--- gerenciador/models.py
+++ gerenciador/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Cursos(models.Model):
+    nome_curso = models.CharField(max_length=100)
+    valor_matricula = models.IntegerField()
+    data_de_inicio = models.DateField(auto_now=True)
+    hora_da_aula = models.TimeField()
+    imagem_curso = models.ImageField(upload_to="gerenciador/images/%Y/%m/%d/")
+    
+    def __str__(self) -> str:
+        return self.nome_curso
+
+class Alunos(models.Model):
+    nome_aluno = models.CharField(max_length=100)
+    curso = models.ForeignKey(Cursos,on_delete=models.SET_NULL, null=True)
+    funcionario = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.nome_aluno
